0dad99b46bf3312d92864376cc0893277319eb83
abdurrezzakekidi <abdurrezzakekid82@gmail.com>
2024-01-03 23:17:03 +0300

    Upd: login and register layout

--- src/HesapOlustur.java
+++ src/HesapOlustur.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class HesapOlustur extends JFrame {
     JLabel LhesapOLustur =new JLabel("HESAP OLUŞTUR");
@@ -6,6 +7,7 @@ public class HesapOlustur extends JFrame {
     JButton hesapOLusturB =new JButton("HESAP OLUŞTUR");
     JButton jButOnayla= new JButton("ONAYLA");
     JButton jButGeri= new JButton("GERİ");
+    JLabel hesapOlusturLabel = new JLabel("HESAP OLUŞTUR");
 
     JPanel panel2 = new JPanel(null);
     private JTextField adSoyadField = new JTextField(20);
@@ -16,6 +18,24 @@ public class HesapOlustur extends JFrame {
     private JPasswordField sifreOnayField = new JPasswordField(20);
     private JTextField cinsiyetField = new JTextField(20);
     private JTextField emailField = new JTextField(20);
+
+    public HesapOlustur(){
+        this.setTitle(" HESAP OLUŞTUR");
+        this.setLayout(null);
+        this.setSize(1200,800);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        panel1.setBounds(0,0,1200,80);
+        hesapOlusturLabel.setFont(new Font("Inter",Font.BOLD,40));
+       hesapOlusturLabel.setForeground(new Color(0xffffff));
+       hesapOlusturLabel.setBounds(429,0,342,80);
+       hesapOLusturB.setBounds(422,108,356,68);
+
+
+
+    }
 
 
 
